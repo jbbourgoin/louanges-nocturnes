3f2bd0198ed9974ae10cb4beeca05f80a31c1cb5
jbbourgoin <jeanbaptiste.bourgoin@gmail.com>
2013-11-26 22:44:30 +0100

    Suppression des fichiers temporaires

--- compiler.sh
+++ compiler.sh
@@ -5,4 +5,5 @@ makeindex -s index-perso.ist psa &&
 makeindex -s index-perso.ist hym &&
 makeindex -s index-perso.ist orat &&
 xelatex matines.tex &&
+mv matines.pdf out/ &&
 rm ./*.aux ./*.idx ./*.ilg ./*.ind ./*.out ./*.toc ./*.dvi ./psautiers/*.aux
